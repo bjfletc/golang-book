6487b793d736abb8d1a52b57a7e34ce18546ade2
Brandon Fletcher <brandonjfletcher@gmail.com>
2018-09-09 01:49:49 -0500

    Strings...

--- chapter2/main.go
+++ chapter2/main.go
@@ -7,4 +7,8 @@ func main() {
   fmt.Println("1.0 + 1.0 =", 1.0 + 1.0)
 
   fmt.Println("The lenght of 'Hello, World' is :", len("Hello, World"))
+
+  fmt.Println(len("Hello, World!"))
+  fmt.Println("Hello, World!"[1])
+  fmt.Println("Hello, " + "World!")
 }
